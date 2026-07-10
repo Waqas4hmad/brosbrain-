ce15729fc7159a151d314950a23f8ee96210613c
Waqas4hmad <waqa5.4hmad@gmail.com>
2026-07-10 06:52:05 +0100

    resolve

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
+  turbopack: {
+    root: __dirname, // Force root to your project folder
   },
+  outputFileTracingRoot: __dirname, // Fix tracing too
 };
 
 module.exports = nextConfig;
